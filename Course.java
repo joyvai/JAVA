f9c1b75840d8985b6f873da2a327855669235c43
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-10-17 22:45:50 -0700

    lab code

--- Course.java
+++ Course.java
@@ -1,32 +1,39 @@
 public class Course{
 	private String courseName;
-	private double courseFee;
+	//private double courseFee;
 	private int courseCredit;
+	static final int courseFee = 5000; // static means i use the variable value but
+	// don't modify the value
+	// i can access this variable from any class
+	// because it is static variable
+	
 
 	public Course(){
-		this ("Java",110000,2);
+		//this ("Java",110000,2);
 		System.out.println("Right now there is no subject");
 	}
-	public Course(String subjectName, double subjectFee, int credit){
+	public Course(String subjectName, int credit){
 		courseName = subjectName;
-		courseFee = subjectFee;
+		//courseFee = subjectFee;
 		courseCredit=credit;
 	}
 	
 	public void setCourse(String subject){
 		courseName = subject;
 	}
-	public void setCourseFee(double money){
-		courseFee = money;
-	}
+	//public void setCourseFee(double money){
+		//courseFee = money;
+	//}
 	public void courseCredit(int credit) { courseCredit=credit; }
 	
 	public String getCourseName(){
 		return courseName;
 	}
+	/*
 	public double getCourseFee(){
 		return courseFee;
 	}
+	*/
 	public int getCourseCredit(){
 		return courseCredit;
 	}
@@ -36,5 +43,6 @@ public class Course{
 		System.out.println("Your course fee:"+ courseFee);
 		System.out.println("Your course credit:"+ courseCredit);
 	}
+	
 }
 
