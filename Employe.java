5205386ff3a821ffe9012b4191471ee42c989485
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-10-20 23:39:14 -0700

    Employe inheritance example

---
+++ Employe.java
@@ -0,0 +1,12 @@
+class Programmer{
+	float salary=4000;
+}
+class Employe extends Programmer{
+	int bonus = 10000;
+
+	public static void main(String[] args){
+		Employe p = new Employe();
+		System.out.println("Programmer salary is: "+p.salary);
+		System.out.println("Bonus of programmer is:"+p.bonus);
+	}
+}
